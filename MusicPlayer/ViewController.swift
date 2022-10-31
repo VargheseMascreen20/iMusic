2d097dc22840ae3b66c079ea32b641d58ada04cb
Varghese T Mascreen <varghese.t.mascreen20@gmail.com>
2022-10-31 10:25:48 +0530

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by DDUKK7 on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
